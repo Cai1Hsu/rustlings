f60d190b6d3732db2acbc6921188d11234f96a7e
Caiyi Hsu <cai1hsu@outlook.com>
2024-04-10 23:10:20 +0800

    Hello `privitive_types` 1 and 2

--- exercises/primitive_types/primitive_types2.rs
+++ exercises/primitive_types/primitive_types2.rs
@@ -1,6 +1,6 @@
 // primitive_types2.rs
 //
-// Fill in the rest of the line that has code missing! No hints, there's no
+// Fill in the rest of he line thatt has code missing! No hints, there's no
 // tricks, just get used to typing these :)
 
 // I AM NOT DONE
@@ -10,20 +10,28 @@ fn main() {
 
     // Note the _single_ quotes, these are different from the double quotes
     // you've been seeing around.
-    let my_first_initial = 'C';
-    if my_first_initial.is_alphabetic() {
-        println!("Alphabetical!");
-    } else if my_first_initial.is_numeric() {
-        println!("Numerical!");
-    } else {
-        println!("Neither alphabetic nor numeric!");
-    }
+    // let my_first_initial = 'C';
+    // if my_first_initial.is_alphabetic() {
+    //     println!("Alphabetical!");
+    // } else if my_first_initial.is_numeric() {
+    //     println!("Numerical!");
+    // } else {
+    //     println!("Neither alphabetic nor numeric!");
+    // }
 
-    let // Finish this line like the example! What's your favorite character?
+    // Finish this line like the example! What's your favorite character?
+    let your_character: char = 'a'; // alpha
+    let your_character: char = '1'; // an number
+    let your_character: char = '#'; // an symbol
+    let your_character: char = '\u{001b}'; // an escape char
+    let your_character: char = '字'; // an Chinese character
+    let your_character: char = 'ア'; // an Japanese character
+
     // Try a letter, try a number, try a special character, try a character
     // from a different language than your own, try an emoji!
     if your_character.is_alphabetic() {
-        println!("Alphabetical!");
+        // WTF? is_alphabetic() thought ア is alphabetic???
+        println!("Alphabetical! {}", your_character);
     } else if your_character.is_numeric() {
         println!("Numerical!");
     } else {
