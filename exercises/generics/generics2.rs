48c319d67249eb4690d49aa08b34a2ee815e6c30
Caiyi Hsu <cai1hsu@outlook.com>
2024-04-11 20:26:16 +0800

    finish `generics`

--- exercises/generics/generics2.rs
+++ exercises/generics/generics2.rs
@@ -6,14 +6,12 @@
 // Execute `rustlings hint generics2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
-struct Wrapper {
-    value: u32,
+struct Wrapper<TValue> {
+    value: TValue,
 }
 
-impl Wrapper {
-    pub fn new(value: u32) -> Self {
+impl<TValue> Wrapper<TValue> {
+    pub fn new(value: TValue) -> Self {
         Wrapper { value }
     }
 }
